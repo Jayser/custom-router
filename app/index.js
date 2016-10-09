8c08bed5fb2836907fb45a3562664a920430b77c
Sarhan.Azizov <sarhan.azizov@gmail.com>
2016-10-09 23:53:07 +0300

    feature-1: add api methods

--- app/index.js
+++ app/index.js
@@ -5,6 +5,38 @@ import router from './utils/router';
 console.log(router);
 window.router = router;
 
+/* this.add('/gallery1/:tag/:perPage/page/:page/', function () {
+ console.log(arguments);
+ });
+
+ this.add(/^\/gallery2\/(\w+)\/(\w+)\/page\/(\w+)\/$/, function () {
+ console.log(arguments);
+ });
+
+ this.go('/gallery1/t/tt/page/ttt/');
+ this.go('/gallery2/t/tt/page/ttt/');*/
+
+router.add('/gallery', function () {
+    console.log('>>> /gallery');
+});
+
+router.add('/gallery/:id', function (data) {
+    console.log('>>> /gallery/' + data[0]);
+});
+
+router.go('/gallery');
+router.go('/gallery/id1');
+router.go('/gallery/id2');
+router.back();
+//router.go('/notFound');
+/*router.notFound(() => {
+    console.log('Not Found');
+});
+
+router.go('/notFound');
+
+console.log('parse', router.parse('/gallery/id2'));*/
+
 render(
     React.createElement('div', null, 'text'),
     document.getElementById('app')
--- app/utils/router.js
+++ app/utils/router.js
@@ -1,100 +1,89 @@
 class Router {
     constructor() {
-        this.routers = [];
+        this._routers = [];
         this._init();
     }
 
-    _init() {
-        this.addHandleOnPopState();
+    // Utils
+    _isFunction(type) {
+        return typeof type === 'function';
+    }
 
-        this.on({
-            url: 'test.html',
-            handle: () => console.log('handle'),
-            after: () => console.log('after'),
-            before: () => console.log('before'),
-            middleware: () => true
+    _isString(type) {
+        return typeof type === 'string';
+    }
+
+    _isRegExp(type) {
+        return type instanceof RegExp;
+    }
+
+    // Private
+    _init() {
+        this._addHandleOnPopState();
+    }
+
+    _addHandleOnPopState() {
+        addEventListener('popstate', ({ state = {} }) => this.go(state.url), false);
+    }
+
+    _match(str, regexp) {
+        return str.match(regexp);
+    }
+
+    _findRoute(url) {
+        return this._routers.find(({ pattern }) => this._match(url, pattern));
+    }
+
+    _toPattern(url) {
+        return new RegExp(`^${url.replace(/:\w+/g, '(\\w+)')}$`);
+    }
+
+    _addRoute({ handle, pattern }, url) {
+        let result = false;
+        const args = this._match(url, pattern);
+
+        if (this._isFunction(handle)) {
+            history.pushState({ pattern }, null, url);
+            handle(args && args.slice(1));
+            result = true;
+        }
+
+        return result;
+    }
+
+    // Public
+    add(url, handle) {
+        if ((this._isString(url) || this._isRegExp(url)) && this._isFunction(handle)) {
+            const pattern = this._isRegExp(url) ? url : this._toPattern(url);
+
+            this._routers.push({ pattern, handle });
+        }
+    }
+
+    go(url) {
+        const route = this._findRoute(url);
+
+        if(route) {
+            return this._addRoute(route, url);
+        }
+
+        this._notFound && this._notFound();
+    }
+
+    parse(url) {
+        return this._routers.filter(route => {
+            return this._match(url, route.pattern);
         });
     }
 
-    addHandleOnPopState() {
-        window.addEventListener('popstate', e => {
-            console.log('triggered popstate ', e);
-            }, false);
+    back() {
+        history.back();
     }
 
-    _findRouteByUrl(val) {
-        return this.routers.find(rout => rout.url === val);
+    notFound(fn) {
+        this._notFound = fn;
     }
 
-    on(opt) {
-        const hasRoute = this._findRouteByUrl(opt.url);
-
-        if (!hasRoute) {
-            this.routers.push(opt);
-            return true;
-        }
-
-        return false;
-    }
-
-    to(url) {
-        const route = this._findRouteByUrl(url);
-
-        if(route && typeof route.handle === 'function') {
-            if (typeof route.after === 'function') {
-                console.log('after', true);
-                route.after();
-            }
-
-            if (typeof route.middleware === 'function') {
-                if (route.middleware()) {
-                    console.log('middleware ', true);
-                    history.pushState({ route: route.url }, null, route.url);
-                    route.handle();
-                } else {
-                    console.log('middleware ', false);
-                    return false;
-                }
-            } else {
-                console.log('handle', true);
-                history.pushState({ route: route.url }, null, route.url);
-                route.handle();
-            }
-
-            if (typeof route.before === 'function') {
-                console.log('before', true);
-                route.before();
-            }
-
-            return true;
-        }
-    }
 }
-
-/*
-* on:
-
- }
-
- prev () {
-
- }
-
- is () {
-
- }
-
- destroy () {
-
- }
-
- link () {
-
- }
-
- notFound () {
-
- }
-* */
 
 export default new Router();
